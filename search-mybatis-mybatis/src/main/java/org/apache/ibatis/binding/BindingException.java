a8f57e9d06cb33970b1c0099a1c58c444ff9949c
heweiweihe <heweiweihe@gmail.com>
2016-05-06 15:35:42 +0800

    Update BindingException.java

--- search-mybatis-mybatis/src/main/java/org/apache/ibatis/binding/BindingException.java
+++ search-mybatis-mybatis/src/main/java/org/apache/ibatis/binding/BindingException.java
@@ -13,6 +13,9 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+ /**
+  * 中文注释
+  */
 package org.apache.ibatis.binding;
 
 import org.apache.ibatis.exceptions.PersistenceException;
